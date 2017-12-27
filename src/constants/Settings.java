9577e695b16a8673c060b342accd157bdb8a0894
Massy BOURENNANI <cm_bourennani@esi.dz>
2017-12-27 17:48:51 +0100

    init commit

---
+++ src/constants/Settings.java
@@ -0,0 +1,21 @@
+package constants;
+
+public class Settings {
+
+	//public static final String rootProject="/Users/adi/Dropbox/TEACHING/DDM/WWW/WS-evaluation/";
+	public static final String rootProject="./WS-Evaluation/";
+	public static final String dirWithDef=rootProject+"ws-definitions/";
+
+	
+	
+	public static final String getDirForCallResults(String ws){
+		return rootProject+ws+"/call_results/";
+	}
+	
+	public static final String getDirForTransformationResults(String ws){
+		return rootProject+ws+"/transf_results/";
+	}
+	
+	
+	
+}
